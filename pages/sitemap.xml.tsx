fbb23c1eac6d1229d8913d404a64530217f21fe3
marianmolocea <marian.mcrt@gmail.com>
2022-02-05 22:02:25 +0000

    create sitemap amnnualy

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -5,51 +5,27 @@ const Sitemap = () => {
 }
 
 export async function getServerSideProps({ res }: any) {
-  try {
-    const staticPaths = fs
-      .readdirSync('pages')
-      .filter((staticPage) => {
-        return ![
-          'api',
-          'product',
-          '_app.tsx',
-          '_document.tsx',
-          '404.tsx',
-          'sitemap.xml.tsx',
-        ].includes(staticPage)
-      })
-      .map((staticPagePath) => {
-        const pathName = staticPagePath.split('.')[0]
-        const pageName = pathName === 'index' ? '' : pathName
-        return `${process.env.NEXT_PUBLIC_BASE_URL}/${pageName}`
-      })
+  const sitemap = ` <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://www.easypercentage.net/</loc>
+      <lastmod>2022-02-05T21:55:03.374Z</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://www.easypercentage.net/privacy</loc>
+      <lastmod>2022-02-05T21:55:03.374Z</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  </urlset>`
 
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${staticPaths
-        .map((url, index) => {
-          return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>${index === 0 ? '1.0' : '0.8'}</priority>
-            </url>
-          `
-        })
-        .join('')}
-    </urlset>
-  `
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
 
-    res.setHeader('Content-Type', 'text/xml')
-    res.write(sitemap)
-    res.end()
-
-    return {
-      props: {},
-    }
-  } catch (error) {
-    console.log(error)
+  return {
+    props: {},
   }
 }
 
